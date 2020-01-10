b1eba4231d825a0a5fbf80a9f4bc83ac6c970912
Negorizont <boltuev01@mail.ru>
2020-01-10 17:49:21 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  const stringArray = expression.split('*');
+
+  let fiveCount = 0,
+      twoCount = 0,
+      step = 0;
+
+  stringArray.forEach(function(stringNum) {
+    
+    let intNum = parseInt(stringNum);
+
+    (stringNum.includes('!!')) ? step = 2 : step = 1;
+
+    for (let i = 0; i < intNum; intNum -= step) {
+      let currentNumber = intNum;
+
+      while (currentNumber > 0) {
+        if (currentNumber % 5 == 0) {
+          fiveCount++; 
+        }
+
+        currentNumber /= 5;
+      }
+
+      currentNumber = intNum;
+
+      while (currentNumber > 0) {
+        if (currentNumber % 2 == 0) {
+          twoCount++; 
+        }
+
+        currentNumber /= 2;
+      }
+    }
+
+  });
+
+  return fiveCount > twoCount ? twoCount : fiveCount;
+};
